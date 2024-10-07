d81c32bffad31959f73013eaf2c17dd244850ef1
imon-sheikh <mdimonhossain2017@gmail.com>
2024-10-07 20:16:25 +0600

    added markas read

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-const loadAllPosts = async(category) => {
+const loadAllPosts = async (category) => {
+  // console.log(`https://openapi.programming-hero.com/api/retro-forum/posts${category? `?category=${category}` : "" }`);
 
-    // console.log(`https://openapi.programming-hero.com/api/retro-forum/posts${category? `?category=${category}` : "" }`);
-    
-   
-// if(category){
-//     console.log(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`);
-// }else{
-//     console.log(`https://openapi.programming-hero.com/api/retro-forum/posts`);
-// }
- 
-    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${category? `?category=${category}` : "" }`)
-    const data = await response.json()
-    displayAllPosts(data.posts);    
-}
+  // if(category){
+  //     console.log(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${category}`);
+  // }else{
+  //     console.log(`https://openapi.programming-hero.com/api/retro-forum/posts`);
+  // }
 
-const displayAllPosts = (posts) =>{
-// console.log(posts);
-const postContainer = document.getElementById("post-container")
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/retro-forum/posts${
+      category ? `?category=${category}` : ""
+    }`
+  );
+  const data = await response.json();
+  displayAllPosts(data.posts);
+};
 
-posts.forEach(post => {
+const displayAllPosts = (posts) => {
+  // console.log(posts);
+  const postContainer = document.getElementById("post-container");
+
+  posts.forEach((post) => {
     // console.log(post);
-    const div = document.createElement('div')
+    const div = document.createElement("div");
 
     div.innerHTML = `
           <div class="p-6 lg:p-12 flex gap-6 lg:flex-row flex-col items-center lg:items-start bg-[#F3F3F5]">
 
             <div class="indicator">
               <span class="indicator-item badge ${
-                post.isActive ? 'bg-green-500' : 'bg-red-500' 
+                post.isActive ? "bg-green-500" : "bg-red-500"
               }"></span>
               <div class="avatar">
                 <div class="w-24 rounded-xl">
@@ -70,7 +72,7 @@ posts.forEach(post => {
                 <div class="opacity-100">
                   <button
                   data-post="" 
-                  id="addToList" onclick="markAsRead()" class="addToList btn btn-circle bg-green-500 btn-sm">
+                  id="addToList" onclick="markAsRead('${post.description}', '${post.view_count}')" class="addToList btn btn-circle bg-green-500 btn-sm">
                     <i class="fa-solid fa-envelope-open"></i>
                   </button>
                 </div>
@@ -78,21 +80,41 @@ posts.forEach(post => {
               </div>
             </div>
            </div>
+    `;
+
+    postContainer.appendChild(div);
+  });
+};
+
+
+
+const markAsRead = (description, view_count) =>{
+    // console.log(description, view_count);
+    const markAsReadContainer = document.getElementById('markAsReadContainer')
+    const div = document.createElement('div')
+    div.innerHTML = `
+           <div class="flex justify-between p-2 lg:p-3 bg-white rounded-2xl items-center gap-3">
+                   <div class="lg:w-4/5 w-11/12">
+                      <p>${description}</p>
+                   </div>
+                   <div class="lg:w-1/5 w-4/12 flex justify-end">
+                     <p>
+                      <i class="fa-regular fa-eye"></i>
+                      ${view_count}
+                     </p>
+                   </div>
+                 </div>   
     `
 
-    postContainer.appendChild(div)
-})
-
+    markAsReadContainer.appendChild(div)
 }
 
-loadAllPosts()
+loadAllPosts();
 
 //handle search
-const handleSearchByCategory = () =>{
-    const searchText = document.getElementById('searchPosts').value
-    console.log(searchText);
+const handleSearchByCategory = () => {
+  const searchText = document.getElementById("searchPosts").value;
+  console.log(searchText);
 
-    loadAllPosts(searchText)  
-}
-
-
+  loadAllPosts(searchText);
+};
